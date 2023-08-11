99f2c6bec0e5f8b5445b5fd137cd8524b0e67384
Syawal Achmad <syawalachmad1323@gmail.com>
2023-08-11 22:44:52 +0800

    Initial commit

--- main.ts
+++ main.ts
@@ -1,2 +1,19 @@
 // TODO: Change this function to make all the tests pass
-export function stair(num) {}
+export function stair(num: number) {
+  const result: string[][] = [];
+
+  for (let i = 1; i <= num; i++) {
+    const row: string[] = [];
+
+    for (let j = 1; j <= i - 1; j++) {
+      row.push(" ");
+    }
+
+    for (let k = i; k <= num; k++) {
+      row.push("*");
+    }
+
+    result.push(row);
+  }
+  return result;
+}
